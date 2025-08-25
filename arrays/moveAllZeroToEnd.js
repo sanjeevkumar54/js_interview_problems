5411eb1d7fe50084a327ec6c99beaf1e159f440c
sanjeevkumar54 <sanjeev.kumar2@programming.com>
2025-08-25 16:00:41 +0530

    Update moveAllZeroToEnd.js

--- arrays/moveAllZeroToEnd.js
+++ arrays/moveAllZeroToEnd.js
@@ -17,3 +17,17 @@ while(arr.length > nz ){
     }
 }
 console.log(arr)
+//solution with for loop
+
+function moveZeroToEndStable(arr) {
+    let nz = 0; // index to place next non-zero
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            [arr[nz], arr[i]] = [arr[i], arr[nz]]; // swap
+            nz++;
+        }
+    }
+    return arr;
+}
+
+console.log(moveZeroToEndStable([0,0,1,2,3])); // [1,2,3,0,0]
